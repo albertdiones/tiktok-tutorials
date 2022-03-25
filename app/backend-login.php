383b9265b45341f5e9a1c43f51e585a120764eee
albert <albert@addteam.org>
2022-03-25 17:47:48 +0800

    executable lamp script

--- app/backend-login.php
+++ app/backend-login.php
@@ -10,5 +10,5 @@ if (sha1($_POST['password']) === '53b6db49372383e6fb48873bc741a5d6e43f80a0') {
     echo "Password is correct user:".$_POST['username'];
 }
 else {
-    throw new \Exception("Invalid password");
+    throw new \Exception("Invalid password!!!");
 }
