0dd85ea8bc28d580ed4c18a7fbd889ba141f172c
albert <albert@addteam.org>
2022-03-26 17:28:40 +0800

    actual working login

--- app/backend-login.php
+++ app/backend-login.php
@@ -5,8 +5,24 @@ set_exception_handler(
     }
 );
 
+$mysqli = mysqli_connect(
+    "localhost",
+    "albert",
+    "IcedCoffee"
+);
+
+$mysqli->select_db("earth");
+
+$query = $mysqli->prepare("SELECT * FROM persons WHERE email= ?");
+
+$query->bind_param('s',$_POST['username']);
+$query->execute();
+$result = $query->get_result();
+
+$user = $result->fetch_assoc();
+
 # 53b6db49372383e6fb48873bc741a5d6e43f80a0
-if (sha1($_POST['password']) === '53b6db49372383e6fb48873bc741a5d6e43f80a0') {
+if (sha1($_POST['password']) === $user['sha1_password']) {
     echo "Password is correct user:".$_POST['username'];
 }
 else {
