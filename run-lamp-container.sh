ab2bc98f74dd5afe80784348d7e08f1d2a97f964
albert <albert@addteam.org>
2022-03-29 22:45:18 +0800

    usable dockerfile

--- run-lamp-container.sh
+++ run-lamp-container.sh
@@ -1,1 +1,6 @@
-docker run -p "80:80" -p 3306:3306 -v ${PWD}/app:/app -v ${PWD}/mysql:/var/lib/mysql mattrayner/lamp:latest-1804
+docker build -t tiktok_tuts .
+docker run\
+  -p 8006:80 -p 33060:3306\
+  -v ${PWD}/app:/app\
+  -v ${PWD}/mysql:/var/lib/mysql\
+  tiktok_tuts
