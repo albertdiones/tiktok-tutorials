ca488cf3c3bca113b71909ebd6d7a541f071465b
albert <albert@addteam.org>
2022-04-07 17:44:19 +0800

    randomize listing ajax

--- app/listing.php
+++ app/listing.php
@@ -1,9 +1,15 @@
 <?php
-    sleep(2);
+    sleep(1);
+    $developers = [
+        'DMCI',
+        'Cityland',
+        'SMDC',
+        'Caliraya'
+    ];
 ?>
 <div class="listing">
-    <img src="dmci-unit.jpeg" width="270px" height="auto" />
-    <h3>DMCI Condo in Makati</h3>
+    <img src="CondoUnits/<?= mt_rand(1,5) ?>.jpeg" width="270px" height="auto" />
+    <h3><?= $developers[mt_rand(0,count($developers)-1)] ?> Condo in Makati</h3>
     <p>This condo is in the middle of nowhere that is good for your health</p>
     <small>PHP 13,000,0000</small>
 </div>
