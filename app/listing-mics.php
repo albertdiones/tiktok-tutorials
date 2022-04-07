78e0051583c137770841639ddea47d4913d33cd3
albert <albert@addteam.org>
2022-04-07 17:53:02 +0800

    url argument for different listing url

---
+++ app/listing-mics.php
@@ -0,0 +1,9 @@
+<?php
+    sleep(mt_rand(1,3));
+?>
+<div class="listing">
+    <img src="Houses/d4fde72d2627e3.jpg" width="270px" height="auto" />
+    <h3>Micara House and Lot in Makati</h3>
+    <p>This house is the house that you'll like. It has all your needs</p>
+    <small>PHP 13,000,0000</small>
+</div>
